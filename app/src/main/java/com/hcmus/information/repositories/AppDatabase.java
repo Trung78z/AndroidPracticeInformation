c08c00e8ab4118831f70e7edd4f76dc86308854e
trung78z <trungpspy@gmail.com>
2025-05-18 10:00:23 +0700

    rename db

--- app/src/main/java/com/hcmus/information/repositories/AppDatabase.java
+++ app/src/main/java/com/hcmus/information/repositories/AppDatabase.java
@@ -20,7 +20,7 @@ public abstract class AppDatabase extends RoomDatabase {
             synchronized (AppDatabase.class) {
                 if (INSTANCE == null) {
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
-                                    AppDatabase.class, "my-database")
+                                    AppDatabase.class, "information")
                             .build();
                 }
             }
